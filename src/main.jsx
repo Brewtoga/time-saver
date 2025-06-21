18206699be9d098db3da7f2a9ef051314ecc31ec
George Brewton <60475383+Brewtoga@users.noreply.github.com>
2025-06-21 09:18:49 -0400

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -2,21 +2,6 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import { Amplify } from 'aws-amplify'
-import outputs from '../amplify_outputs.json'
-
-// Configure Amplify only if outputs exist
-async function configureAmplify() {
-  try {
-    const { default: outputs } = await import('../amplify_outputs.json')
-    Amplify.configure(outputs)
-    console.log('Amplify configured')
-  } catch (error) {
-    console.log('No Amplify backend configured')
-  }
-}
-
-configureAmplify()
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
